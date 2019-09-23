2c2af9efcf26ea6c6bcc9434237f657fff44c2e1
Igor Tavarer <iv_tavares@yahoo.com.br>
2019-09-23 00:39:27 -0300

    Section 3: 10. Criando e modelando a app de Localização

--- core/models.py
+++ core/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from atracoes.models import Atracao
+from comentarios.models import Comentario
+from avaliacoes.models import Avaliacao
+from enderecos.models import Endereco
 
-# Create your models here.
+
 class pontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+    atracao = models.ManyToManyField(Atracao)
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacao = models.ManyToManyField(Avaliacao)
+    endereco = models.ForeignKey(Endereco)
 
     def __str__(self):
         return self.nome
